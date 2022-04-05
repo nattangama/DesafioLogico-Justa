018db320303704505ca0f863fa61abe00fa58521
Nattan <91083215+nattangama@users.noreply.github.com>
2022-04-05 16:59:39 -0300

    teste

---
+++ FristProgram.java
@@ -0,0 +1,19 @@
+package DesafioJusta;
+
+import java.util.Scanner;
+
+public class Logica{
+
+    public static void main (String[] args) {
+
+        Scanner entrada = new Scanner (System.in);
+        System.out.println ("Digite a idade: ")
+        int idI = entrada.nextInt();
+        System.out.println("Digite a idade: ")
+        int idII = entrada.nextInt();
+        if (idI < idII)
+        System.out.println("A idade " + idI +" é menor que " + idII );
+    }else 
+        System.out.println("A idade "+ idII + "é menor que " + idI)
+    }
+}
