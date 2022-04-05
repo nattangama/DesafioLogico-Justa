ce7737324ecfa97d26b562e4ee36603338b151d3
Nattan <91083215+nattangama@users.noreply.github.com>
2022-04-05 17:02:24 -0300

    pessoa

---
+++ Segundo Programa.java/people.java
@@ -0,0 +1,18 @@
+package desafio;
+
+classe pública Pessoa {
+	Nome corda;
+	Sexo cordas ;
+	int idade;
+	
+	vazio público maiorDeIdade(int idade) {
+        Scanner entrada = new Scanner (System.in);
+        System.out.println ("Digite a idade: ")
+		if (idade>=18) {
+			sistema.out.println ("Maior de idade".);
+		}else {
+			sistem.out.println ("Menor de idade".);
+		}	 
+	}
+
+}
